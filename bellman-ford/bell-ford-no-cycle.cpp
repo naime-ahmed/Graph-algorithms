803029b4dd9ffb3f3ca44f7d1b9e0f00e606fe5a
dev-naime <naime2molla@gmail.com>
2023-01-13 23:19:48 +0600

    added some info in bellman-ford

--- bellman-ford/bell-ford-no-cycle.cpp
+++ bellman-ford/bell-ford-no-cycle.cpp
@@ -9,6 +9,15 @@ int d[N];
 /*
 This Algorithm will work for only graph which hasn't any negative cycle.
 
+input: Graph with bo negative cycle
+4 5
+1 2 1
+2 4 1
+3 1 1
+4 1 3
+4 3 -2
+output: 0 1 0 2
+
 */
 int main()
 {
@@ -27,8 +36,9 @@ int main()
         adj_list[u].push_back({v, w});
     }
 
+    // consider 1 as a source node
     int src = 1;
-
+    // make the distance of source node as 0
     d[src] = 0;
 
     for (int i = 1; i <= n - 1; i++)
